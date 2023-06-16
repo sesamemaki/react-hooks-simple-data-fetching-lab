75cf105db53e0f4a8e6cb893cb848ad4cd5a28e2
Burcu Bayraö <burju7@yahoo.com>
2023-06-16 12:19:27 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogs, setDogs] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogs(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  // if the data hasn't been loaded, show a loading indicator
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={dogs} alt="A Random Dog" />;
+}
+
+export default App;
